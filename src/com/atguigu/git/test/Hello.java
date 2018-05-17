b3beecce20efffdaa7f907af488f0fe25eff0daf
fangxinde <fangxinde@rsdalstar-rd.com>
2018-05-17 09:10:22 +0800

    local更新03

--- src/com/atguigu/git/test/Hello.java
+++ src/com/atguigu/git/test/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
 	  System.out.println("2222");
           System.out.println("githup更新01");
           System.out.println("local更新02"); 
+          System.out.println("local更新03"); 
 	}
 }
