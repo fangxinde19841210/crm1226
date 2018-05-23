528ce7201c797b1bba50a103eac3cc0c69af60d9
fangxinde <fangxinde@rsdalstar-rd.com>
2018-05-23 10:43:22 +0800

    冲突解决01

--- src/com/atguigu/git/test/Hello.java
+++ src/com/atguigu/git/test/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
           System.out.println("local更新01");    
 		System.out.println("远程冲突01");  
+			 System.out.println("本地冲突01"); 
 	}
 }
